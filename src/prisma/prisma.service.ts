417d6f0e24800b30272486820d2fa354e143a287
leSchb <kurultae@yandex.ru>
2026-01-01 04:30:57 +0300

    update prisma servicce

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,18 +3,6 @@ import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const options: any = {
-      errorFormat: 'pretty',
-      log: [],
-    };
-
-    if (process.env.PRISMA_ACCELERATE_URL?.trim()) {
-      options.accelerateUrl = process.env.PRISMA_ACCELERATE_URL;
-    }
-    super(options);
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
